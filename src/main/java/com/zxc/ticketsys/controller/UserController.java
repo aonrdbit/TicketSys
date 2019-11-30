4cc88a78d4516bb57d0a5033d37b2c9ba7b0931c
Zeng1998 <1129142694@qq.com>
2019-11-30 23:07:04 +0800

    add token

--- src/main/java/com/zxc/ticketsys/controller/UserController.java
+++ src/main/java/com/zxc/ticketsys/controller/UserController.java
@@ -2,21 +2,31 @@ package com.zxc.ticketsys.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/user")
 public class UserController{
-    @RequestMapping(value = "/login",method = RequestMethod.GET)
+    @RequestMapping(value = "/login",method = RequestMethod.POST)
     @ResponseBody
-    public String login() throws JsonProcessingException {
+    public String login(@RequestHeader Map<String,Object> he,@RequestBody Map<String,Object> para) throws JsonProcessingException {
+        System.out.println(he);
+        String username=(String)para.get("username");
+        String password=(String)para.get("password");
         HashMap<String,Object> hs=new HashMap<>();
-        hs.put("token","token");
+        hs.put("token","token"+username+password);
+        ObjectMapper objectMapper=new ObjectMapper();
+        return objectMapper.writeValueAsString(hs);
+    }
+
+    @RequestMapping(value = "/test",method = RequestMethod.GET)
+    @ResponseBody
+    public String test(@RequestHeader Map<String,Object> he) throws JsonProcessingException {
+        System.out.println(he);
+        HashMap<String,Object> hs=new HashMap<>();
         ObjectMapper objectMapper=new ObjectMapper();
         return objectMapper.writeValueAsString(hs);
     }
