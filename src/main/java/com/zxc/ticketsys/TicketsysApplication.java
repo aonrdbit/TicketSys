fec4b4281864beda6b6e3f1e5f2e90d6a5ac9592
Zeng1998 <1129142694@qq.com>
2019-12-01 20:27:19 +0800

    update

--- src/main/java/com/zxc/ticketsys/TicketsysApplication.java
+++ src/main/java/com/zxc/ticketsys/TicketsysApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TicketsysApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(TicketsysApplication.class, args);
     }
-
 }
--- src/main/java/com/zxc/ticketsys/controller/UserController.java
+++ src/main/java/com/zxc/ticketsys/controller/UserController.java
@@ -2,6 +2,8 @@ package com.zxc.ticketsys.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.zxc.ticketsys.model.User;
+import com.zxc.ticketsys.utils.TokenUtil;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
@@ -12,21 +14,21 @@ import java.util.Map;
 public class UserController{
     @RequestMapping(value = "/login",method = RequestMethod.POST)
     @ResponseBody
-    public String login(@RequestHeader Map<String,Object> he,@RequestBody Map<String,Object> para) throws JsonProcessingException {
-        System.out.println(he);
+    public String login(@RequestBody Map<String,Object> para) throws JsonProcessingException {
         String username=(String)para.get("username");
         String password=(String)para.get("password");
+        String token= TokenUtil.sign(new User(username,password));
         HashMap<String,Object> hs=new HashMap<>();
-        hs.put("token","token"+username+password);
+        hs.put("token",token);
         ObjectMapper objectMapper=new ObjectMapper();
         return objectMapper.writeValueAsString(hs);
     }
 
-    @RequestMapping(value = "/test",method = RequestMethod.GET)
+    @RequestMapping(value = "/test",method = RequestMethod.POST)
     @ResponseBody
-    public String test(@RequestHeader Map<String,Object> he) throws JsonProcessingException {
-        System.out.println(he);
+    public String test(@RequestBody Map<String,Object> para) throws JsonProcessingException {
         HashMap<String,Object> hs=new HashMap<>();
+        hs.put("data","data");
         ObjectMapper objectMapper=new ObjectMapper();
         return objectMapper.writeValueAsString(hs);
     }
