00c3e1137ebf7b95c38cb9cbbd510800dbfd4ede
Zeng1998 <1129142694@qq.com>
2019-12-05 16:30:54 +0800

    update

--- src/main/java/com/zxc/ticketsys/utils/TokenUtil.java
+++ src/main/java/com/zxc/ticketsys/utils/TokenUtil.java
@@ -23,7 +23,7 @@ public class TokenUtil {
             Date expiresAt = new Date(System.currentTimeMillis() + EXPIRE_TIME);
             token = JWT.create()
                     .withIssuer("auth0")
-                    .withClaim("username", user.getUsername())
+                    .withClaim("username", user.getUserName())
                     .withExpiresAt(expiresAt)
                     // 使用了HMAC256加密算法。
                     .sign(Algorithm.HMAC256(TOKEN_SECRET));
